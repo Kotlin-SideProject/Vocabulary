9edc6a5c7476da633710cc6eb794f1dd942d106a
Hank Tom <hanktom@gmail.com>
2020-10-17 01:17:19 +0800

    initial commit

---
+++ app/src/main/java/com/litto/vocabulary/data/WordNames.kt
@@ -0,0 +1,10 @@
+package com.litto.vocabulary.data
+
+object WordNames {
+    const val TABLE_NAME = "words"
+    const val COL_ID = "id"
+    const val COL_NAME = "name"
+    const val COL_MEANS = "means"
+    const val COL_STAR = "star"
+
+}
